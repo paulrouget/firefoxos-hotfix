350ea86be4dd09f6cde0a5a55299fa14dafe2bef
Paul Rouget <me@paulrouget.com>
2013-09-30 15:37:58 +0200

    no need to get the profile name

--- update.sh
+++ update.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-PROFILE=a8bj06yh.default
-
 adb shell mount -o rw,remount /system
 
 adb shell stop b2g && \
+mkdir -p tmp &&
 cd tmp && rm -rf * && \
-unzip ../omni.ja.orig ; \
-cp ~/mozilla/src/toolkit/devtools/server/actors/styles.js modules/devtools/server/actors/ && \
+unzip ../omni.ja.orig && \
+cp ~/mozilla/src/toolkit/devtools/server/actors/webapps.js modules/devtools/server/actors/ && \
 rm ../omni.ja ; \
 zip -r ../omni.ja * && \
 cd .. && \
 adb push omni.ja /system/b2g/omni.ja && \
-adb shell rm /data/b2g/mozilla/$PROFILE/startupCache/* ; \
+adb shell 'cd /data/b2g/mozilla/*.default/ && rm startupCache/*' ; \
 adb shell mount -o ro,remount /system && \
 adb shell /system/bin/b2g.sh
 
