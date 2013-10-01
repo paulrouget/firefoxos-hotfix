0e66db28ecbecf9245ec22dfcad15fe110af8689
Paul Rouget <me@paulrouget.com>
2013-10-01 15:22:01 +0200

    update

--- update.sh
+++ update.sh
@@ -3,42 +3,41 @@
 set -o errexit
 
 if [ ! -e "omni.ja.orig" ]; then
-  echo "Not omni.ja.orig file."
-  echo "Pull the original omni.ja from the device"
-  exit 1
+  echo "Pulling omni.jar"
+  adb pull /system/b2g/omni.ja omni.ja.orig
 fi
 
-echo "Create/cleanup a tmp directory"
+echo "Cleanup tmp/ directory"
 rm -rf tmp
 mkdir tmp
 cd tmp
 
-echo "stop B2G"
+echo "stopping B2G..."
 adb shell stop b2g
-echo "Make sure filesystem is rw"
-adb shell mount -o rw,remount /system
 
 echo "Unzip original omni.jar file"
-unzip -q ../omni.ja.orig
+unzip -q ../omni.ja.orig || echo ignoring zip error
 
 # Custom part
 echo "Files udpate"
 cp ~/mozilla/src/toolkit/devtools/server/actors/webapps.js modules/devtools/server/actors/
+cp ../Webapps.jsm modules/
 
 # End custom part
 
 echo "Repackage omni.ja with modifications"
-rm ../omni.ja
+rm -f ../omni.ja
 zip -q -r ../omni.ja *
 cd ..
 
+echo "Make sure filesystem is rw"
+adb shell mount -o rw,remount /system
 echo "Pushing new omni.ja file"
 adb push omni.ja /system/b2g/omni.ja
 echo "Clearing some cache"
-adb shell 'cd /data/b2g/mozilla/*.default/ && echo $PWD && rm -rf startupCache/*'
-
+adb shell 'cd /data/b2g/mozilla/*.default && rm startupCache/*'
 echo "Remount ro"
 adb shell mount -o ro,remount /system
 
-echo "Start B2G"
-adb shell /system/bin/b2g.sh
+echo "Starting B2G..."
+exec adb shell /system/bin/b2g.sh
